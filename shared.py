afbf5bf4fac38ca3d36609a97a2e9f6dd3fa0669
Max <glubonit@gmail.com>
2016-05-13 20:41:30 +0300

    =time

--- shared.py
+++ shared.py
@@ -1,17 +1,15 @@
 from datetime import datetime
 def time_dict(a):
 	s=(a-datetime(2016,1,1)).total_seconds()
-	hours = s // 3600
-	minutes = s%3600 // 60
-	seconds = s%60
-	when={'hours':hours,'minutes':minutes,'seconds':seconds}
-	return when
+
+	return s
 	
 
 def time_diff(a,b):
-	hours = a['hours'] - b['hours']
-	minutes = a['minutes'] - b['minutes']
-	seconds = a['seconds'] - b['seconds']
+	s = a - b
+	hours = s // 3600
+	minutes = s%3600 // 60
+	seconds = s%60
 	result=""
 	if hours>0:
 		result+=str(int(hours))+ " hrs "
