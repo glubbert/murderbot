638880fc5ab0297d13a5c5dab76d4eb79852e379
Max <glubonit@gmail.com>
2016-05-14 16:45:49 +0300

    =tumbles

--- mbgifs.py
+++ mbgifs.py
@@ -3,17 +3,52 @@ import json,urllib,re,traceback
 from random import choice,shuffle
 show_me="^show\s+me\s+(?P<query>.*?)(?:\s+#(?P<index>\d+))?$"
 lewd = "^lewd\s+(?P<what>.+)"
+tumblr="tumblr\s+(?:(?P<type>photo|text)\s+)?(?P<what>.+)"
+html_tags = re.compile(r'<[^>]+>')
 
 
-
+		
+def tumblr_func(nick,match,target):
+	t=match.group("type")
+	if t:
+		types=[t]
+	else:
+		types=["text","photo"]
 	
-
+	what = match.group('what')
+	query = urllib.parse.urlencode({'tag':what})
+	try:
+		req = urllib.request.Request("https://api.tumblr.com/v2/tagged?api_key=oBkePh2f80LWzw0jfO3DEYz2YLfRRCmmz8VM9rsv5cxwa4kGAZ&"+query)
+		response = json.loads(urllib.request.urlopen(req).read().decode('utf-8'))
+	except:
+		mb.tell("whoa there hol up something aint right",target)
+		traceback.print_exc()
+		return
+	posts=response["response"]
+	shuffle(posts)
+	for post in posts:
+		if post["type"] in types:
+			if post["type"] =="photo":
+				photo = choice(post["photos"])["original_size"]["url"]
+				mb.tell(nick+": "+photo,target)
+				mb.tell("post: "+post["short_url"],target)
+				return
+			else:
+				
+				msg=re.sub("\n|\r"," ",re.sub(html_tags,"",post["body"]))
+				mb.tell(msg[:200],target)
+				mb.tell("post: "+post["short_url"],target)
+				return
+				
+	mb.tell(nick+": nope, nothing like that", target)	
+	
+		
 
 def lewd_func(nick,match,target):
 	what = match.group('what')
 	query = urllib.parse.urlencode({'tag':what})
 	
-	tumbles = ['deliciousnights','thoughtsandthoughtsoflove','sweet-loving-sex','schnoez','sexornothing','c-opulation',]
+	tumbles = ['deliciousnights','porn-gifs','petitecoquiine','schnoez','sexornothing','c-opulation',]
 	shuffle(tumbles)
 	posts = []
 	
@@ -23,7 +58,7 @@ def lewd_func(nick,match,target):
 	
 	for tumble in tumbles:
 		try:
-			req = urllib.request.Request("https://api.tumblr.com/v2/blog/"+tumble+".tumblr.com/posts/photo?api_key=fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4&"+query)
+			req = urllib.request.Request("https://api.tumblr.com/v2/blog/"+tumble+".tumblr.com/posts/photo?api_key=oBkePh2f80LWzw0jfO3DEYz2YLfRRCmmz8VM9rsv5cxwa4kGAZ&"+query)
 			response = json.loads(urllib.request.urlopen(req).read().decode('utf-8'))
 		except:
 			mb.tell("whoa there hol up something aint right",target)
@@ -39,6 +74,7 @@ def lewd_func(nick,match,target):
 				photo = choice(post['photos'])['original_size']['url']
 				if re.search(".gif$",photo):
 					mb.tell(nick+": "+photo,target)
+					mb.tell("post: "+post["short_url"],target)
 					return
 		
 	mb.tell(nick+": none of that here, what is wrong with you",target)	
@@ -55,10 +91,6 @@ def lewd_func(nick,match,target):
 
 def show_me_func(nick,match,target):
 	index = match.group('index')
-	if not index:
-		index = 0
-	else:
-		index = int(index)
 	headers={'X-Mashape-Key': '6SRh5ZIyhhmshOjLLIEVlfRzZR3Mp1KJgLsjsny2Vq36opmhI6',
 			'Accept':'application/json'}
 	query=urllib.parse.urlencode({"q":match.group('query')})
@@ -70,17 +102,24 @@ def show_me_func(nick,match,target):
 	if results==[]:
 		mb.tell(nick+": none of that stuff", target)
 		return
-	data=results[index]
+	if index:
+		data=results[int(index)]
+	else:
+		data=choice(results)
+	
 	gif="https://media.giphy.com/media/{id}/giphy.gif".format(id=data['id'])
 
 	mb.tell(nick+": "+gif,target,True)
 
-		
+	
+
 		
 mb.add_command(show_me,show_me_func,priority=3)
-mb.add_command(lewd,lewd_func,priority=3)
+mb.add_command(lewd,lewd_func)
+mb.add_command(tumblr,tumblr_func)
+mb.help["gifs"]="mb show me <whatever> (searches for <whatever> on giphy), mb lewd <whatever> (searches for gifs in \\tagged of several porn tumblrs)"
 
-mb.help["gifs"]="mb show me <whatever> (searches for <whatever> on giphy), mb lewd <whatever> (searches on gif-porn.net)"
+mb.help["tumblr"]="mb tumblr [text|photo](optional) <something>"
 
 print("loaded gifs")
 
