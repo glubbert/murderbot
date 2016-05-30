745881e824ebc7bb3f7c57182250654a6b2abc94
Max <glubonit@gmail.com>
2016-05-30 21:13:50 +0300

    =imp

--- mbot.py
+++ mbot.py
@@ -1,99 +1,52 @@
+import sys,traceback,irc,logging,imp,os
+homedir = os.path.dirname(os.path.realpath('__file__'))
+path="no path :("
+paths = ["D:/murderbot","/home/batlord/murderbot/murderbot",homedir]
+for p in paths:
+	if os.path.isfile(os.path.join(p,"mbclient.py")):
+		path = p
+		break
+
+print("the path is: "+path)	
+
+sys.path.append(path)
 from mbclient import mb
-import sys,traceback,irc,logging
-try:
-	import mbcommands
-except:
-	print("failed to import commands :(")
-	traceback.print_exc()
-try:
-	import mbcolor
-except:
-	print("failed to import colors :(")
-	traceback.print_exc()
-try:
-	import mbtranslation
-except:
-	print("failed to import translation :(")
-	traceback.print_exc()
-try:
-	import mbdictionary
-except:
-	print("failed to import dictionary :(")
-	traceback.print_exc()
-try:
-	import mbbing
-except:
-	print("failed to import bing search :(")
-	traceback.print_exc()
-try:
-	import mbwhow
-except:
-	print("failed to import wikihow :(")
-	traceback.print_exc()
-try:
-	import mbquiz
-except:
-	print("failed to import quiz :(")
-	traceback.print_exc()
-try:
-	import mbconvert
-except:
-	print("failed to import conversion :(")
-	traceback.print_exc()
-try:
-	import mbquotes
-except:
-	print("failed to import quotes :(")
-	traceback.print_exc()
-try:
-	import mbmisc
-except:
-	print("failed to import misc :(")
-	traceback.print_exc()
 
-try:
-	import mbcalc
-except:
-	print("failed to import calculator :(")
-	traceback.print_exc()	
+def load(modname):
 
-try:
-	import mbupdates
-except:
-	print("failed to import updates :(")
-	traceback.print_exc()	
-try:
-	import mbgifs
-except:
-	print("failed to import gifs :(")
-	traceback.print_exc()	
-try:
-	import mbinterview
-except:
-	print("failed to import interview :(")
-	traceback.print_exc()	
-try:
-	import mbrecognition
-except:
-	print("failed to import recognition :(")
-	traceback.print_exc()	
-try:
-	import mbpassive
-except:
-	print("failed to import passive functions :(")
-	traceback.print_exc()	
-try:
-	import mbwiki
-except:
-	print("failed to import wiki :(")
-	traceback.print_exc()	
-try:
-	import mburban
-except:
-	print("failed to import urban :(")
-	traceback.print_exc()	
+	handle, pathname, descrip = imp.find_module(modname, [path])
+	try:
+		return imp.load_module(modname, handle, pathname, descrip)
+	except:
+		print("failed to import "+modname+" :(")
+		traceback.print_exc()
 	
+modnames=[
+	"mbcolor",
+	"mbcommands",
+	"mbtranslation",
+	"mbbing",
+	"mbwhow",
+	"mbquiz",
+	#"mbconvert",
+	"mbquotes",
+	"mbmisc",
+	"mbcalc",
+	#"mbupdates",
+	"mbgifs",
+	"mbinterview",
+	"mbrecognition",
+	"mbpassive",
+	"mbwiki",
+	"mburban",
+	"mbdictionary",
+	]
+for module in modnames:
+	load(module)
+
 if __name__=="__main__":
+	
 	#logging.basicConfig(filename="murder.log", level=logging.DEBUG)
 	mbot = mb()
+	print("{} commands".format(len(mb.commands)))
 	mbot.start()
